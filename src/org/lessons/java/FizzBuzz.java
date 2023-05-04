62b6da974c04e4a7361154eb5b0bc09249ffeaa0
AntoninoIrrera <irrera.antonio2003@gmail.com>
2023-05-04 17:51:32 +0200

    update fix bonus fizz buzz

--- src/org/lessons/java/FizzBuzz.java
+++ src/org/lessons/java/FizzBuzz.java
@@ -4,7 +4,31 @@ import java.util.Scanner;
 
 public class FizzBuzz {
 	public static void main(String[] args) {
-		for (int i = 1; i <= 100; i++) {
+		
+		
+//		metodo senza bonus
+		
+//		for (int i = 1; i <= 100; i++) {
+//			if(i % 15 == 0) {
+//				System.out.println("FizzBuzz");
+//			}else if(i % 3 == 0) {
+//				System.out.println("Fizz");
+//			}else if(i % 5 == 0) {
+//				System.out.println("Buzz");
+//			}else {
+//				System.out.println(i);
+//			}
+//		}
+		
+		
+//		metodo con bonus
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Inserisci un numero: ");
+		int userNumber = sc.nextInt();     
+		
+		for (int i = 1; i <= userNumber; i++) {
 			if(i % 15 == 0) {
 				System.out.println("FizzBuzz");
 			}else if(i % 3 == 0) {
@@ -14,21 +38,6 @@ public class FizzBuzz {
 			}else {
 				System.out.println(i);
 			}
-		}
-		
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.print("Inserisci il numero da valutare: ");
-		int userNumber = sc.nextInt();     
-		
-		if(userNumber % 15 == 0) {
-			System.out.println("FizzBuzz");
-		}else if(userNumber % 3 == 0) {
-			System.out.println("Fizz");
-		}else if(userNumber % 5 == 0) {
-			System.out.println("Buzz");
-		}else {
-			System.out.println(userNumber);
 		}
 		
 		sc.close();
